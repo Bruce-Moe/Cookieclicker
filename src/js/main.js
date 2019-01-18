3b795c736dd0cd007cfaa4ea6354f9a35d136e2f
LucasG234 <lucasgates234@gmail.com>
2019-01-18 14:34:30 -0600

    Changed hardcoded values into array

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 var score = 0; // Define global variable to hold score
-var autoclick = 0; //Autoclick upgrade variable
-var farms = 0;  //Farm upgrade variable
+var clickers = [0, 0]; //Autoclick, farm
+var costMult = [12, 45];
+var scoreMult = [1, 5];
 
 //Update index.html text elements function
 function update() {
@@ -8,17 +9,17 @@ function update() {
   score_div.innerHTML = 'Score: ' + score; // Update the score in the DOM
 
   var amount_div = document.getElementsByClassName('amountAutoClick')[0];
-  amount_div.innerHTML= "You Own " + autoclick + " Autoclickers";
+  amount_div.innerHTML= "You Own " + clickers[0] + " Autoclickers";
   var cost_div = document.getElementsByClassName('CostAutoClick')[0];
-  cost_div.innerHTML= "Cost " + ((autoclick+1) * 12) + " Cookies";
+  cost_div.innerHTML= "Cost " + ((clickers[0]+1) * costMult[0]) + " Cookies";
 
   var amountfarms_div = document.getElementsByClassName("amountFarms")[0];
-  amountfarms_div.innerHTML = "You Own " + farms + " Farms";
+  amountfarms_div.innerHTML = "You Own " + clickers[1] + " Farms";
   var costfarms_div = document.getElementsByClassName("costFarm")[0];
-  costfarms_div.innerHTML= "Cost " + ((farms+1) * 45) + " Cookies";
+  costfarms_div.innerHTML= "Cost " + ((clickers[1]+1) * costMult[1]) + " Cookies";
 
   var cookiespersec_div = document.getElementsByClassName("persecond")[0];
-  cookiespersec_div.innerHTML= "You are gaining " + (((autoclick)+(farms*5))*multiplier) + " Cookies per/s";
+  cookiespersec_div.innerHTML= "You are gaining " + (((clickers[0]*scoreMult[0])+(clickers[1]*scoreMult[1]))*multiplier) + " Cookies per/s";
 }
 
 //multiplier variable
@@ -26,8 +27,8 @@ var multiplier = 1;
 
 //timer function
 function timer() {
-  score = score + autoclick;
-  score = score + farms*5;
+  score = score + clickers[0]*scoreMult[0];
+  score = score + clickers[1]*scoreMult[1];
   update();
 }
 
@@ -52,8 +53,8 @@ init(); // Call the init function
 //save function
 function save() {
   localStorage.setItem("score", score);
-  localStorage.setItem("autoclick", autoclick);
-  localStorage.setItem("farms", farms);
+  localStorage.setItem("autoclick", clickers[0]);
+  localStorage.setItem("farms", clickers[1]);
 }
 
 //load function
@@ -65,27 +66,19 @@ function load() {
   var score_div = document.getElementsByClassName('score')[0];
   score_div.innerHTML = 'Score: ' + score; // Update the score in the DOM
 
-  autoclick = localStorage.getItem("autoclick");
-  autoclick = parseInt(autoclick);
+  clickers[0] = localStorage.getItem("autoclick");
+  clickers[0] = parseInt(clickers[0]);
 
-  farms = localStorage.getItem("farms");
-  farms = parseInt(farms);
+  clickers[1] = localStorage.getItem("farms");
+  clickers[1] = parseInt(clickers[1]);
 }
 
-//Buy farm function
-function buyFarm() {
-  if (score >= ((farms+1)*45)) {
-    score = score - ((farms+1)*45);
-    farms = farms + 1;
-    update();
-  }
-}
+function buy( index ) {
 
-//Buy autoclick function
-function buyAutoClick() {
-  if (score >= ((autoclick+1) * 12)) {
-    score = score - ((autoclick+1) * 12);
-    autoclick = autoclick + 1;
+  if( score >= ( ( clickers[index] + 1 ) * costMult[index] ) ) {
+    score = score - ( clickers[index] + 1 ) * costMult[index];
+    clickers[index] = clickers[index] + 1;
     update();
   }
+
 }
